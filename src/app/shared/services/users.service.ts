b5d2c394b5b2020b84322e2ec88323833b9fba78
unknown <phytsyk@gmail.com>
2018-05-12 21:56:00 +0300

    fix registration

--- src/app/shared/services/users.service.ts
+++ src/app/shared/services/users.service.ts
@@ -13,12 +13,12 @@ export class UsersService extends BaseApi{
 
   getUserByEmail(email: string): Observable<User> {
     return this.get(`users?email=${email}`)
-      .map((user: User) => user[0] ? user[0]: undefined )
+      .map((user: User) => user[0] ? user[0] : undefined)
   }
 
 
   createNewUser(user: User): Observable<User> {
-    return this.post('services', user)
-      .map((response: Response) => response.json());
+    return this.post('users', user)
+      .flatMap((response: Response) => this.getUserByEmail(user.email));
   }
 }
